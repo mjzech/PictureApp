ef618cd0bcbccffae7c64c25a26eb9ce523ef953
Zech <mzech@dhs.state.ia.us>
2018-10-31 14:28:14 -0500

    Initial Commit

---
+++ PictureApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PictureApp.API.Models;
+
+namespace PictureApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+            public DbSet<Value> Values { get; set; }
+        
+    }
+}
