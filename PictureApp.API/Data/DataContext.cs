ad31dbedd1f0e7ff6224c0448b99a24233d4e125
Zech <mzech@dhs.state.ia.us>
2018-11-01 11:13:39 -0500

    Update

--- PictureApp.API/Data/DataContext.cs
+++ PictureApp.API/Data/DataContext.cs
@@ -7,6 +7,7 @@ namespace PictureApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
             public DbSet<Value> Values { get; set; }
+            public DbSet<User> Users{ get; set;}
         
     }
 }
