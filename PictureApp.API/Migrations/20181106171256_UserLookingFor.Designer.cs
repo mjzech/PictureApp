2aa485bf5c6e587506ffe1c3bde929f1e355c793
Zech <mzech@dhs.state.ia.us>
2018-11-06 14:36:50 -0600

    End of section 9

--- PictureApp.API/Migrations/20181106171256_UserLookingFor.Designer.cs
+++ PictureApp.API/Migrations/20181106171256_UserLookingFor.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PictureApp.API.Data;
 
 namespace PictureApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20181106171256_UserLookingFor")]
+    partial class UserLookingFor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PictureApp.API/Models/User.cs
+++ PictureApp.API/Models/User.cs
@@ -18,6 +18,7 @@ namespace PictureApp.API.Models
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string LookingFor { get; set; }
         public ICollection<Photo> Photos {get; set;}    //The reciprical relation from the photo class
     }
 }
